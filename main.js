620ab7487fa3617c1b2e4df0e2773ac787ff78c5
baltimor <ivansldtkn@gmail.com>
2020-12-24 23:53:53 +0500

    part2

--- main.js
+++ main.js
@@ -9,13 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько вы его оцените?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько вы его оцените?','');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько вы его оцените?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;   
+        console.log('Done!');
+    } else {
+        console.log('error');
+        i--;                      // возвращаем назад чтобы задавать вопрос пока условие не выполниться
+    }   
+}
 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классичекий зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+} 
 
 console.log(personalMovieDB);
