d75408315d67bfc3b559bb43dd4663fc365f6fe1
baltimor <ivansldtkn@gmail.com>
2021-01-02 17:27:54 +0500

    part4

--- main.js
+++ main.js
@@ -1,72 +1,69 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        do {
+             this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        } while (this.count == '' || this.count == null || isNaN(this.count));
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?',''),
+                  b = prompt('На сколько вы его оцените?','');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                this.movies[a] = b;   
+                console.log('Done!');
+            } else {
+                console.log('error');
+                i--;                   // возвращаем назад чтобы задавать вопрос пока условие не выполниться
+            }   
+        }
+    },
+    decectPersonalLevel: function() {
+        if (this.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (this.count >= 10 && this.count < 30) {
+            console.log('Вы классичекий зритель');
+        } else if (this.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        } 
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(this);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 4; i++) {
+            do {
+                this.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '').toLowerCase();
+            } while (!this.genres[i - 1]);            
+        }
+        this.genres.sort();
+        this.genres.forEach((item, i) => {
+            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if (this.privat) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    }
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?',''),
-              b = prompt('На сколько вы его оцените?','');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;   
-            console.log('Done!');
-        } else {
-            console.log('error');
-            i--;                   // возвращаем назад чтобы задавать вопрос пока условие не выполниться
-        }   
-    }
-}
-
-rememberMyFilms();
-
-function decectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классичекий зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    } 
-}
-
-decectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
-    }
-}
-
-writeYourGenres();
-
-
-
-
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.decectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
